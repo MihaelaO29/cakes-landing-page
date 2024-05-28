652e348ccfcf1b60ed9a90122eefeecf54e40da4
MihaelaO29 <opriamihaela22@yahoo.com>
2024-05-28 20:00:46 +0300

    creating app

---
+++ script.js
@@ -0,0 +1,7 @@
+let image = document.querySelector(".content");
+let images = ['cake1.jpg', 'cake2.jpg', 'cake3.jpg', 'cake4.jpg']
+
+setInterval(function () {
+   let random = Math.floor(Math.random() * 4);
+   image.style.backgroundImage = `url(./img/${images[random]}`;
+}, 1300); 
