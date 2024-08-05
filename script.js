41d75c6458022aea0fd849342604103e23686837
MihaelaO29 <opriamihaela22@yahoo.com>
2024-08-05 19:44:45 +0300

    changed timer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let image = document.querySelector(".content");
-let images = ['cake1.jpg', 'cake2.jpg', 'cake3.jpg', 'cake4.jpg']
+let images = ['cake1.jpg', 'cake2.jpg', 'cake3.jpg', 'cake4.jpg'];
 
 setInterval(function () {
-   let random = Math.floor(Math.random() * 4);
-   image.style.backgroundImage = `url(./img/${images[random]}`;
-}, 1500); 
+   let random = Math.floor(Math.random() * images.length);
+   image.style.backgroundImage = `url(./img/${images[random]})`;
+}, 1500);
